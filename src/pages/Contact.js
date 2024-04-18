494974c1da64828c2ba24c8405b47cb54fd35f5c
Ujer123 <ujermohd0@gmail.com>
2024-04-18 17:12:56 +0530

    responsive

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -55,7 +55,7 @@ export default function Contact(){
     };
 
     return(
-        <>
+        <section className="mb-4">
             <div className='about'> 
                 <div className='about-text text-8xl font-bold text-white flex justify-content-center'>Contact</div>  
             </div>
@@ -63,7 +63,7 @@ export default function Contact(){
             <div className='lg:grid lg:flex justify-content-around'>
 
                 <div className='lg:col-6 col-11 '>
-                    <h2 className="ml-4">Get In Touch</h2>
+                    <h2 className="ml-4 val-txt">Get In Touch</h2>
                     <div className="card flex justify-content-center">
                         <form onSubmit={formik.handleSubmit} className="flex flex-column gap-2">
                             <span className="p-float-label">
@@ -104,23 +104,23 @@ export default function Contact(){
                             {getFormErrorMessage('mobile')}
 
                             <span className="p-float-label">
-                            <InputTextarea value={value} onChange={(e) => setValue(e.target.value)} rows={5} cols={30} />
+                            <InputTextarea value={value} onChange={(e) => setValue(e.target.value)} rows={5} cols={30} className="msg-inp"/>
                             <label htmlFor="message">Type Message</label>
                             </span>
                             
-                            <Button type="submit" label="Submit" className="text-orange-500 bg-white border-orange-500 border-rounded-3xl sub-but"/>
+                            <Button type="submit" label="Submit" className='bg-white text-orange-500 border-orange-500 border-round-3xl sub-but'/>
                         </form>
                     </div>
                 </div>
 
                 <div className='lg:col-6 col-11  '>
                     <h2>Our Address</h2>
-                    <p><i className="pi pi-building bg-black-alpha-50 p-1 mr-2"></i>2130 Fulton Street, San Diego, USA</p>
-                    <p><i className="pi pi-phone bg-black-alpha-50 p-1 mr-2"></i>1-800-1234-567</p>
-                    <p><i className="pi pi-envelope bg-black-alpha-50 p-1 mr-2"></i> info@demolink.org</p>
+                    <p><i className="pi pi-building text-white bg-black-alpha-90 p-1 mr-2"></i>2130 Fulton Street, San Diego, USA</p>
+                    <p><i className="pi pi-phone text-white bg-black-alpha-90 p-1 mr-2"></i>1-800-1234-567</p>
+                    <p><i className="pi pi-envelope text-white bg-black-alpha-90 p-1 mr-2"></i> info@demolink.org</p>
                     <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15092.899608597789!2d72.8310554!3d18.9656724!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7cfdf5cadca43%3A0x2f248f63460fcd4f!2sHeuristic%20Academy!5e0!3m2!1sen!2sin!4v1712155173082!5m2!1sen!2sin" allowfullscreen="" loading="lazy" title="map" referrerpolicy="no-referrer-when-downgrade" style={{height:'50%', width:'80%'}}/>
                 </div>
             </div>
-        </>
+        </section>
     );
 }
